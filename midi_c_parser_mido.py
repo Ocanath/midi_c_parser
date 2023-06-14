21c30bfe28e3616bc48e9f3522348386954f87f2
ocanath <ocanath@gmail.com>
2023-06-13 23:23:52 -0700

    working in the style that it would be parsed on an actual system. i was being a total dumbass. this parser is good though

--- midi_c_parser_mido.py
+++ midi_c_parser_mido.py
@@ -16,65 +16,97 @@ def get_note_from_idx(idx):
 	taken from tone.c. modified to remove octave as an argument
 """
 def get_note_freq(step):
-	base = 1.05946309
-	exp = 1.0
-	for i in range(0,step):
-		exp = base*exp
-	return 16.35*exp
+	# base = 1.05946309
+	# exp = 1.0
+	# for i in range(0,step):
+		# exp = base*exp
+	# return 16.35*exp
+	return 16.35*(1.059463094359)**step
 
 note_lookup = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 
-mid = MidiFile('ChickenFried.mid')
+# mid = MidiFile('ChickenFried.mid')
+mid = MidiFile('46and2.mid')
 # mid = MidiFile('Bass_sample.mid')
 print(mid.ticks_per_beat)
 
-notelist = []
 for i, track in enumerate(mid.tracks):
 	print("Track "+str(i), str(track.name))
 	for  msg in track:
 		if(msg.is_meta == 0):
 			# print(msg)
 			pass
-	
-trackidx = 1
-track = mid.tracks[trackidx]
-print('Track {}: {}'.format(trackidx, track.name))
-
-
-time = 0
+			
 notelist = []
-for i, msg in enumerate(track):
-	if("note_on" in str(msg)):
-		time = time + msg.time
-		if(msg.velocity > 0):
+tracks = [3]
+for trackidx in tracks:	
+	
+	track = mid.tracks[trackidx]
+	print('Track {}: {}'.format(trackidx, track.name))
+
+	time = 0
+	for i, msg in enumerate(track):
+		if("note_on" in str(msg) or "note_off" in str(msg)):
+			time = time + msg.time
 			note,octave = get_note_from_idx(msg.note)
-			for j in range(i,len(track)):
-				if(track[j].is_meta == 0 and (("note_on" in str(track[j])) or ("note_off" in str(track[j]))) ):
-					if(track[j].note == msg.note and ( (("note_on" in str(track[j])) and track[j].velocity == 0) or ("note_off" in str(track[j])) )  ):
-						notelist.append([msg.note, time, time+track[j].time])
-						print(note_lookup[note]+str(octave), "(" + str(note+12*octave) + ")", "start time = ", time, "end time = ", time+track[j].time, "duration = ", track[j].time, msg.velocity)
-						break
+			
+			on_off = 1
+			if(msg.velocity == 0) or ("note_off" in str(msg)):
+				on_off = 0
+			
+			notelist.append([msg.note, time, on_off])
+			print(note_lookup[int(np.mod(msg.note,12))]+str(octave), "(" + str(msg.note) + ")", "time = ", time, "velocity: ", msg.velocity)
+
+def retstart(e):
+	return e[1]
+notelist.sort(key=retstart)
 		
-				
+
 Fs = 48e3
-endtick = notelist[len(notelist)-1][2]
+endtick = notelist[len(notelist)-1][1]
 endtime = endtick/mid.ticks_per_beat
 t = np.linspace(0, endtime, int(endtime*Fs))
-output = 0*t
+output = t*0
 
 print(endtime)
 
-
+notestate = np.zeros(88)
+notefreq = np.zeros(88)
+for i in range(0,len(notefreq)):
+	notefreq[i] = get_note_freq(i)
+	
+notelist_idx = 0
+# min_note = 88
+# max_note = 0
 for tidx in range(0,len(t)):
-	time = t[tidx]
-	for i in range(0,len(notelist)):
-		st = notelist[i][1]/mid.ticks_per_beat
-		et = notelist[i][2]/mid.ticks_per_beat
-		if(time > st and  time < et):
-			output[tidx]+=np.sin(2*np.pi*time*get_note_freq(notelist[i][0]))
 
-to_wav(t,output,'nylon_chicken.wav')
+	time = t[tidx]
+	ts = notelist[notelist_idx][1]/mid.ticks_per_beat
+	on_off = notelist[notelist_idx][2]
+	note = notelist[notelist_idx][0]
+	# if(note > max_note):
+		# max_note = note
+	# if(note < min_note):
+		# min_note = note
+		
+	if(note > 0 and note < 88):
+		if(time >= ts):
+			if(on_off):
+				notestate[note] = 1
+			else:
+				notestate[note] = 0
+			notelist_idx = notelist_idx + 1
+			print("evt "+str(notelist_idx)+" out of "+str(len(notelist)))
+	
+	for i in range(0,len(notestate)):	#try reducing range...
+		if(notestate[i] == 1):
+			# freq = get_note_freq(i)
+			output[tidx] += np.sin(2*np.pi*time*notefreq[i])
+	
+# output[output>1] = 1
+# output[output<-1] = -1
+to_wav(t,output,'48.wav')
 
 fig,ax = plt.subplots()
 ax.plot(t,output)
